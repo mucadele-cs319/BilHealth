9aa4584e02cdfa4ea444c56dd9fcbc87d265d14d
Ved <dev@vedat.xyz>
2022-02-18 18:44:57 +0300

    setupProxy: Add CRA dev proxy to reach dotnet
    Without a proxy, `dotnet run` will result in a CRA development server that can't direct API requests to the ASP.NET Core backend. This 'sensitive' proxy solves the issue.

---
+++ ClientApp/src/setupProxy.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// API calls can't reach the ASP.NET Core backend without this proxy.
+// For details, see https://create-react-app.dev/docs/proxying-api-requests-in-development/#configuring-the-proxy-manually
+
+/*
+ *
+ *  IGNORE ALL ERRORS IN THIS FILE. CRA SIMPLY DOES NOT SUPPORT ANY OTHER FORMAT.
+ *
+ */
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const { env } = require("process");
+
+const aspnetcore = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(";")[0] : "http://localhost:55892";
+
+module.exports = function (app) {
+    app.use("/api", createProxyMiddleware({
+        target: aspnetcore,
+        secure: false,
+        headers: {
+            Connection: "Keep-Alive"
+        }
+    }));
+};
