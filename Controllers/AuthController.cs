fc392c98120dd1efcd09e6324fe149b8d3cfd83d
Ved <dev@vedat.xyz>
2022-02-18 03:44:25 +0300

    AuthController (WIP): Lay out controller actions with routes

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using BilHealth.Services.Users;
+using BilHealth.Model.Transaction;
+
+namespace BilHealth.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthenticationService AuthenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            AuthenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Register registration)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(Login login)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> LogOut()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
