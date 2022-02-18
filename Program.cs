b6eae97eee789460ee223faef2550cf5fb1896b7
Ved <dev@vedat.xyz>
2022-02-18 03:27:57 +0300

    Program: Configure ASP.NET Core Identity

--- Program.cs
+++ Program.cs
@@ -20,6 +20,24 @@ builder.Services.AddIdentity<User, Role>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
+    options.LoginPath = "/login";
+    options.AccessDeniedPath = "/forbidden"; // This should probably change later
+    options.LogoutPath = "/";
+    options.SlidingExpiration = true;
+}); // May want to use a server-side ticket instead: https://mikerussellnz.github.io/.NET-Core-Auth-Ticket-Redis/
 
 var app = builder.Build();
 
