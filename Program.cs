1de3aafab71fe36d3ab215dc54ffdd8284abe06b
Ved <dev@vedat.xyz>
2022-02-24 21:47:02 +0300

    Program: Prevent redirection on 401/403 responses

--- Program.cs
+++ Program.cs
@@ -33,12 +33,20 @@ builder.Services.Configure<IdentityOptions>(options =>
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
+    options.Cookie.Name = "bilhealthsess";
     options.Cookie.HttpOnly = true;
     options.ExpireTimeSpan = TimeSpan.FromMinutes(15);
-    options.LoginPath = "/login";
-    options.AccessDeniedPath = "/forbidden"; // This should probably change later
-    options.LogoutPath = "/";
     options.SlidingExpiration = true;
+    options.Events.OnRedirectToAccessDenied = context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+        return Task.CompletedTask;
+    };
+    options.Events.OnRedirectToLogin = context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return Task.CompletedTask;
+    };
 }); // May want to use a server-side ticket instead: https://mikerussellnz.github.io/.NET-Core-Auth-Ticket-Redis/
 
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
