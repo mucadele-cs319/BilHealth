6e6c66bc2e9a8ceafec71eb249f6b669b226efd5
Ved <dev@vedat.xyz>
2022-04-06 18:15:04 +0300

    Patient: Add boolean field for blacklisting

--- Model/Identity/Patient.cs
+++ Model/Identity/Patient.cs
@@ -7,8 +7,11 @@ namespace BilHealth.Model
         public double? BodyWeight { get; set; }
         public double? BodyHeight { get; set; }
         public BloodType? BloodType { get; set; }
+
         public List<Vaccination>? Vaccinations { get; set; }
         public List<TestResult>? TestResults { get; set; }
+
         public List<Case>? Cases { get; set; }
+        public bool Blacklisted { get; set; } = false;
     }
 }
