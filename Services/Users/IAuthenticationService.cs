7909edbad921c0c9c514805ccc9089b478e8bbc6
Ved <dev@vedat.xyz>
2022-04-06 18:17:31 +0300

    Chore: Unnecessary public modifiers in interfaces
    The default modifier is public already.

--- Services/Users/IAuthenticationService.cs
+++ Services/Users/IAuthenticationService.cs
@@ -8,17 +8,17 @@ namespace BilHealth.Services.Users
 {
     public interface IAuthenticationService
     {
-        public Task<IdentityResult> Register(Registration registration);
-        public Task RegisterMany(IList<Registration> registrations);
-        public Task<SignInResult> LogIn(Login login);
-        public Task LogOut();
+        Task<IdentityResult> Register(Registration registration);
+        Task RegisterMany(IList<Registration> registrations);
+        Task<SignInResult> LogIn(Login login);
+        Task LogOut();
 
-        public Task CreateRoles();
-        public Task<IdentityResult> AssignRole(User user, UserRoleType roleType);
-        public Task<IdentityResult> AssignRole(string userName, UserRoleType roleType);
+        Task CreateRoles();
+        Task<IdentityResult> AssignRole(User user, UserRoleType roleType);
+        Task<IdentityResult> AssignRole(string userName, UserRoleType roleType);
 
-        public Task<IdentityResult> DeleteUser(string userName);
-        public Task<IdentityResult> ChangePassword(User user, string currentPassword, string newPassword);
-        public Task<User> getUser(ClaimsPrincipal principal);
+        Task<IdentityResult> DeleteUser(string userName);
+        Task<IdentityResult> ChangePassword(User user, string currentPassword, string newPassword);
+        Task<User> getUser(ClaimsPrincipal principal);
     }
 }
--- Services/Users/INotificationService.cs
+++ Services/Users/INotificationService.cs
@@ -1,23 +1,30 @@
 using BilHealth.Model;
-using BilHealth.Model.Dto;
-using BilHealth.Utility.Enum;
 
 namespace BilHealth.Services.Users
 {
     public interface INotificationService
     {
-        public void AddNewAppointmentNotification(Guid userId, Appointment appointment);
-        public void AddAppointmentTimeChangeNotification(Guid userId, Appointment appointment);
-        public void AddAppointmentCancellationNotification(Guid userId, Appointment appointment);
-        public Task AddNewCaseMessageNotification(CaseMessage message);
-        public void AddCaseClosedNotification(Guid userId, Case _case);
-        public void AddCaseTriagedNotification(Guid userId, Case _case);
-        public void AddCaseDoctorChangedNotification(Guid userId, Case _case);
-        public void AddNewPrescriptionNotification(Guid userId, Prescription prescription);
+        void AddNewAppointmentNotification(Guid userId, Appointment appointment);
+        void AddAppointmentTimeChangeNotification(Guid userId, Appointment appointment);
+        void AddAppointmentCancellationNotification(Guid userId, Appointment appointment);
+        /// <summary>
+        /// Add a new notification for a new message.
+        /// <br/>
+        /// If the message is from a Patient and the case has an assigned Doctor, the notification is given to the Doctor.
+        /// If the message is from a Doctor, the notification is given to the Patient.
+        /// Otherwise, both Patient and Doctor (if assigned) are given a notification.
+        /// </summary>
+        /// <param name="message">The new message</param>
+        /// <returns>Completed task after DB queries</returns>
+        Task AddNewCaseMessageNotification(CaseMessage message);
+        void AddCaseClosedNotification(Guid userId, Case _case);
+        void AddCaseTriagedNotification(Guid userId, Case _case);
+        void AddCaseDoctorChangedNotification(Guid userId, Case _case);
+        void AddNewPrescriptionNotification(Guid userId, Prescription prescription);
 
-        public Task MarkNotificationRead(Guid notificationId);
-        public Task MarkAllNotificationsRead(User user);
-        public Task<List<Notification>> GetUnreadNotifications(User user);
-        public Task<List<Notification>> GetAllNotifications(User user);
+        Task MarkNotificationRead(Guid notificationId);
+        Task MarkAllNotificationsRead(User user);
+        Task<List<Notification>> GetUnreadNotifications(User user);
+        Task<List<Notification>> GetAllNotifications(User user);
     }
 }
