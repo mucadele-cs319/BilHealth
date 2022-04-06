b768befa9036aabf076714a2ef175374d5867288
Ved <dev@vedat.xyz>
2022-04-06 18:20:04 +0300

    AnnouncementService: Implement rudimentary service

--- Services/AnnouncementService.cs
+++ Services/AnnouncementService.cs
@@ -1,13 +1,55 @@
 using System.Security.Claims;
+using BilHealth.Data;
 using BilHealth.Model;
 using BilHealth.Model.Dto;
 using BilHealth.Utility.Enum;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace BilHealth.Services
 {
-    public class AnnouncementService : IAnnouncementService
+    public class AnnouncementService : DbServiceBase, IAnnouncementService
     {
+        public AnnouncementService(AppDbContext dbCtx) : base(dbCtx)
+        {
+        }
 
+        public async Task AddAnnouncement(AnnouncementDto announcement)
+        {
+            var newAnnouncement = new Announcement {
+                DateTime = DateTime.Now,
+                Title = announcement.Title,
+                Message = announcement.Message
+            };
+            DbCtx.Announcements.Add(newAnnouncement);
+            await DbCtx.SaveChangesAsync();
+        }
+
+        public async Task<bool> RemoveAnnouncement(Guid announcementId)
+        {
+            var announcement = await DbCtx.Announcements.FindAsync(announcementId);
+            if (announcement is null)
+                return false;
+
+            DbCtx.Announcements.Remove(announcement);
+            await DbCtx.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> UpdateAnnouncement(AnnouncementDto announcement)
+        {
+            var existingAnnouncement = await DbCtx.Announcements.SingleOrDefaultAsync(a => a.Id == announcement.Id);
+            if (existingAnnouncement is null)
+                return false;
+
+            existingAnnouncement.Title = announcement.Title;
+            existingAnnouncement.Message = announcement.Message;
+            await DbCtx.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<List<Announcement>> GetAllAnnouncements()
+        {
+            return await DbCtx.Announcements.OrderByDescending(a => a.DateTime).ToListAsync();
+        }
     }
 }
