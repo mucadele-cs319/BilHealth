8f84d9b3d0e40b95bda221166b24876ff741506a
Ved <dev@vedat.xyz>
2022-04-23 00:20:03 +0300

    Chore: Add Prettier command to package.json (and do a sweep)

--- ClientApp/src/components/pages/NotFound.tsx
+++ ClientApp/src/components/pages/NotFound.tsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React from "react";
 
 const NotFound = (): JSX.Element => {
   return (
     <div id="notfound-container">
       <p>Page not found.</p>
     </div>
-  )
+  );
 };
 
 export default NotFound;
