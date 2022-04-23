a0b3b6c2164e48721f88f01405597cbd2c211ddc
Ved <dev@vedat.xyz>
2022-04-23 06:37:07 +0300

    LoginPage: Implement rudimentary login page

--- ClientApp/src/components/pages/LoginPage.tsx
+++ ClientApp/src/components/pages/LoginPage.tsx
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import AnnouncementList from "./AnnouncementList";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import LoginIcon from "@mui/icons-material/Login";
+import LoadingButton from "@mui/lab/LoadingButton";
+import APIClient from "../../util/API/APIClient";
+import { useNavigate } from "react-router-dom";
 
-const LoginPage = (): JSX.Element => {
-  return <div id="login-container"></div>;
+const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const validate = () => userName.length > 2 && password.length > 4;
+
+  const handleLogin = async () => {
+    setError(false);
+    setIsLoading(true);
+    if (await APIClient.authentication.login({ userName, password, rememberMe })) navigate("/");
+    else setError(true);
+    setIsLoading(false);
+  };
+
+  return (
+    <Box className="flex">
+      <Box className="w-1/2 h-screen py-10 overflow-auto" sx={{ scrollbarWidth: "thin" }}>
+        <Typography variant="h4" gutterBottom textAlign="center">
+          Announcements
+        </Typography>
+        <AnnouncementList />
+      </Box>
+      <Box className="w-1/2 h-screen bg-cyan-500 shadow-lg shadow-black flex">
+        <Paper elevation={8} className="p-5 w-2/5 h-fit m-auto">
+          <Typography variant="h5" gutterBottom>
+            BilHealth - Login
+          </Typography>
+          <TextField
+            id="input-username"
+            label="Username"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            onChange={(e) => setUserName(e.target.value)}
+            error={error}
+          />
+          <TextField
+            id="input-password"
+            type="password"
+            label="Password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            onChange={(e) => setPassword(e.target.value)}
+            error={error}
+          />
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox onChange={(e) => setRememberMe(e.target.checked)} />}
+              label="Remember me"
+            />
+          </FormGroup>
+          <LoadingButton
+            loading={isLoading}
+            disabled={!validate()}
+            variant="contained"
+            loadingPosition="start"
+            fullWidth
+            startIcon={<LoginIcon />}
+            onClick={handleLogin}
+          >
+            Login
+          </LoadingButton>
+        </Paper>
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
