0d141c97749bca9fa22d2c74981d517065b1078b
Ved <dev@vedat.xyz>
2022-04-23 06:35:07 +0300

    AnnouncementList: Polish announcements page with MUI

--- ClientApp/src/components/AnnouncementItem.tsx
+++ ClientApp/src/components/AnnouncementItem.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Announcement } from "../util/API/APITypes";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 interface Props {
   data: Announcement;
+  className?: string;
 }
 
-const AnnouncementItem = ({ data }: Props): JSX.Element => {
+const AnnouncementItem = ({ data, className }: Props) => {
   return (
-    <article>
-      <h3>{data?.title}</h3>
-      <p>{data?.message}</p>
-    </article>
+    <Card className={`max-w-screen-md ${className}`}>
+      <CardContent>
+        <Typography variant="h5">{data.title}</Typography>
+        <Typography variant="subtitle2" color="text.secondary" gutterBottom>
+          {data.dateTime.toString()}
+        </Typography>
+        <Typography variant="body1">{data.message}</Typography>
+      </CardContent>
+    </Card>
   );
 };
 
--- ClientApp/src/components/pages/AnnouncementList.tsx
+++ ClientApp/src/components/pages/AnnouncementList.tsx
@@ -1,27 +1,35 @@
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
 import React, { useEffect, useState } from "react";
 import APIClient from "../../util/API/APIClient";
 import { Announcement } from "../../util/API/APITypes";
 import AnnouncementItem from "../AnnouncementItem";
 
-const Announcements = (): JSX.Element => {
+const Announcements = () => {
   const [announcements, setAnnouncements] = useState<Announcement[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     APIClient.announcements.get().then((response) => {
       setAnnouncements(response);
+      setIsLoaded(true);
     });
   }, []);
 
   return (
-    <div id="announcements-container">
-      <main id="announcements-list">
-        {announcements.length !== 0 ? (
-          announcements.map((announcement, i) => <AnnouncementItem key={i} data={announcement} />)
+    <Box>
+      <Box className="mx-auto w-fit max-w-screen-md">
+        {isLoaded ? (
+          announcements.length !== 0 ? (
+            announcements.map((announcement, i) => <AnnouncementItem key={i} data={announcement} className="mb-5" />)
+          ) : (
+            <span>No announcements at this time.</span>
+          )
         ) : (
-          <span>No announcements at this time.</span>
+          <CircularProgress className="mt-8" />
         )}
-      </main>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
