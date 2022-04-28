22f6591d27b33bf062c1488c3a702e233e6c30cb
Ved <dev@vedat.xyz>
2022-04-28 15:36:03 +0300

    APIClient: Implement profile object

--- ClientApp/src/util/API/APIClient.ts
+++ ClientApp/src/util/API/APIClient.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import { Announcement, Login, Registration, User } from "./APITypes";
+import { Announcement, Login, Registration, SimpleUser, User, Vaccination } from "./APITypes";
 
 const authentication = {
   register: async (registration: Registration) => {
@@ -36,10 +36,57 @@ const authentication = {
 const notifications = {};
 
 const profiles = {
+  all: async (): Promise<SimpleUser[]> => {
+    const response = await fetch("/api/profiles");
+    return await response.json();
+  },
   me: async (): Promise<User> => {
     const response = await fetch("/api/profiles/me");
     if (!response.ok) throw Error("Couldn't get own profile. Am I authenticated?");
     return await response.json();
+  },
+  get: async (userId: string): Promise<User> => {
+    const response = await fetch(`/api/profiles/${userId}`);
+    return await response.json();
+  },
+  update: async (newProfile: User): Promise<void> => {
+    await fetch(`/api/profiles/${newProfile.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProfile),
+    });
+  },
+  blacklist: async (userId: string, state: boolean) => {
+    await fetch(`/api/profiles/${userId}/blacklist?newState=${state}`, {
+      method: "PUT",
+    });
+  },
+  vaccination: {
+    add: async (vaccination: Vaccination): Promise<void> => {
+      await fetch(`/api/profiles/${vaccination.patientUserId}/vaccinations`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(vaccination),
+      });
+    },
+    update: async (vaccination: Vaccination): Promise<void> => {
+      await fetch(`/api/profiles/vaccinations/${vaccination.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(vaccination),
+      });
+    },
+    delete: async (vaccinationId: string): Promise<void> => {
+      await fetch(`/api/profiles/vaccinations/${vaccinationId}`, {
+        method: "DELETE",
+      });
+    }
   },
 };
 
