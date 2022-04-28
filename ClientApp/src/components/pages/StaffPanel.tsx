c2862ee596291fb3ff599a77ca6f51cd448815f2
Ved <dev@vedat.xyz>
2022-04-28 19:35:23 +0300

    StaffPanel: Implement registration card

--- ClientApp/src/components/pages/StaffPanel.tsx
+++ ClientApp/src/components/pages/StaffPanel.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { useDocumentTitle } from "../../util/CustomHooks";
+import Fade from "@mui/material/Fade";
+import Grid from "@mui/material/Grid";
+import RegistrationCard from "../staffpanel/RegistrationCard";
 
 const StaffPanel = () => {
   useDocumentTitle("Staff Panel");
 
-  return <div id="staffpanel-container"></div>;
+  return (
+    <Grid container justifyContent="center">
+      <Fade in={true}>
+        <Grid item lg={10} xs={11}>
+          <RegistrationCard />
+        </Grid>
+      </Fade>
+    </Grid>
+  );
 };
 
 export default StaffPanel;
