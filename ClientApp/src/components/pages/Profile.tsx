3c6fc09f447a61b4d735827e72bee09856a6dc22
Ved <dev@vedat.xyz>
2022-04-29 15:04:45 +0300

    Profile: Basic read-only implementation

--- ClientApp/src/components/pages/Profile.tsx
+++ ClientApp/src/components/pages/Profile.tsx
@@ -1,19 +1,44 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import { useUserContext } from "../UserContext";
+import React, { useEffect, useState } from "react";
 import { useDocumentTitle } from "../../util/CustomHooks";
+import Stack from "@mui/material/Stack";
+import Fade from "@mui/material/Fade";
+import Grid from "@mui/material/Grid";
+import CircularProgress from "@mui/material/CircularProgress";
+import { User } from "../../util/API/APITypes";
+import APIClient from "../../util/API/APIClient";
+import { useParams } from "react-router-dom";
+import ProfileDetails from "../profile/ProfileDetails";
 
 const Profile = () => {
-  const { user } = useUserContext();
-  useDocumentTitle(`${user?.firstName}'s Profile`);
+  useDocumentTitle("Profile");
+
+  const params = useParams();
+  const [queryUser, setQueryUser] = useState<User>();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (params.userid === undefined) throw Error("Couldn't get URL param for user ID");
+    APIClient.profiles.get(params.userid).then(user => {
+      setQueryUser(user);
+      document.title = `${user.firstName}'s ${document.title}`;
+      setIsLoaded(true);
+    });
+  }, [params]);
 
   return (
-    <Box>
-      Temporarily shows signed-in user profile:
-      <pre>
-        <code>{JSON.stringify(user, null, 2)}</code>
-      </pre>
-    </Box>
+    <Grid container justifyContent="center">
+      <Fade in={true}>
+        <Grid item lg={10} xs={11}>
+          {isLoaded ? (
+            <ProfileDetails data={queryUser as User} />
+          ) : (
+            <Stack alignItems="center" className="mt-8">
+              <CircularProgress />
+            </Stack>
+          )}
+        </Grid>
+      </Fade>
+    </Grid>
   );
 };
 
---
+++ ClientApp/src/components/profile/ProfileDetails.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Chip from "@mui/material/Chip";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { stringifyBloodType, stringifyCampus, stringifyGender, User, UserType } from "../../util/API/APITypes";
+import ProfileDetailsItem from "./ProfileDetailsItem";
+
+interface Props {
+  data: User;
+}
+
+const failsafe = (content: string | number | undefined) => {
+  if (typeof content === "number") return content.toString();
+  return content || "—";
+};
+
+const ProfileDetails = ({ data }: Props) => {
+  return (
+    <Card className="max-w-screen-md mb-5 mx-auto">
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          {`${data.firstName} ${data.lastName}`} <Chip className="ml-2" size="small" label={data.userType} />
+        </Typography>
+
+        <Grid container spacing={2} mt={1}>
+          <Grid item>
+            <ProfileDetailsItem title="Email" content={failsafe(data.email)} />
+          </Grid>
+          <Grid item>
+            <ProfileDetailsItem title="Gender" content={stringifyGender(data.gender)} />
+          </Grid>
+          <Grid item>
+            <ProfileDetailsItem title="Date of Birth" content={failsafe(data.dateOfBirth?.toString())} />
+          </Grid>
+        </Grid>
+
+        {data.userType !== UserType.Patient ? null : (
+          <Box mt={2}>
+            <Typography variant="subtitle1">Patient Details</Typography>
+            <Grid container spacing={2}>
+              <Grid item>
+                <ProfileDetailsItem title="Body Weight" content={failsafe(data.bodyWeight)} />
+              </Grid>
+              <Grid item>
+                <ProfileDetailsItem title="Body Height" content={failsafe(data.bodyHeight)} />
+              </Grid>
+              <Grid item>
+                <ProfileDetailsItem title="Blood Type" content={stringifyBloodType(data.bloodType)} />
+              </Grid>
+              <Grid item>
+                <ProfileDetailsItem title="Blacklisted" content={data.blacklisted ? "Yes" : "No"} />
+              </Grid>
+            </Grid>
+          </Box>
+        )}
+
+        {data.userType !== UserType.Doctor ? null : (
+          <Box mt={2}>
+            <Typography variant="subtitle1">Doctor Details</Typography>
+            <Grid container spacing={2}>
+              <Grid item>
+                <ProfileDetailsItem title="Specialization" content={failsafe(data.specialization)} />
+              </Grid>
+              <Grid item>
+                <ProfileDetailsItem title="Campus" content={stringifyCampus(data.campus)} />
+              </Grid>
+            </Grid>
+          </Box>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProfileDetails;
