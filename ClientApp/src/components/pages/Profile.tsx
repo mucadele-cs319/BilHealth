ec591ffef9c558f1a1a7b68883372efe4acee22c
Ved <dev@vedat.xyz>
2022-04-30 20:47:09 +0300

    VaccinationDetails: Add new card to profile

--- ClientApp/src/components/pages/Profile.tsx
+++ ClientApp/src/components/pages/Profile.tsx
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from "react";
-import { useDocumentTitle } from "../../util/CustomHooks";
+import { titleify, useDocumentTitle } from "../../util/CustomHooks";
 import Stack from "@mui/material/Stack";
 import Fade from "@mui/material/Fade";
 import Grid from "@mui/material/Grid";
 import CircularProgress from "@mui/material/CircularProgress";
-import { User } from "../../util/API/APITypes";
+import { User, UserType } from "../../util/API/APITypes";
 import APIClient from "../../util/API/APIClient";
 import { useParams } from "react-router-dom";
 import ProfileDetails from "../profile/ProfileDetails";
+import VaccinationDetails from "../profile/VaccinationDetails";
+import { useUserContext } from "../UserContext";
 
 const Profile = () => {
   useDocumentTitle("Profile");
+
+  const { user } = useUserContext();
 
   const params = useParams();
   const [queryUser, setQueryUser] = useState<User>();
   const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
+  const refreshUser = () => {
     if (params.userid === undefined) throw Error("Couldn't get URL param for user ID");
-    APIClient.profiles.get(params.userid).then(user => {
-      setQueryUser(user);
-      document.title = `${user.firstName}'s ${document.title}`;
+    APIClient.profiles.get(params.userid).then((fetchedUser) => {
+      setQueryUser(fetchedUser);
+      document.title = titleify(`${fetchedUser.firstName}'s Profile`);
       setIsLoaded(true);
     });
+  };
+
+  useEffect(() => {
+    refreshUser();
   }, [params]);
 
   return (
@@ -30,7 +38,17 @@ const Profile = () => {
       <Fade in={true}>
         <Grid item lg={10} xs={11}>
           {isLoaded ? (
-            <ProfileDetails data={queryUser as User} />
+            <>
+              <ProfileDetails data={queryUser as User} />
+              {queryUser?.userType !== UserType.Patient ? null : (
+                <VaccinationDetails
+                  readonly={![UserType.Staff, UserType.Admin].some(type => type === user?.userType)}
+                  refreshHandler={refreshUser}
+                  patientId={queryUser.id as string}
+                  vaccinations={queryUser.vaccinations}
+                />
+              )}
+            </>
           ) : (
             <Stack alignItems="center" className="mt-8">
               <CircularProgress />
---
+++ ClientApp/src/components/profile/VaccinationRow.tsx
@@ -0,0 +1,165 @@
+import IconButton from "@mui/material/IconButton";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import React, { useState } from "react";
+import { Vaccination } from "../../util/API/APITypes";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SaveIcon from "@mui/icons-material/Save";
+import AddIcon from "@mui/icons-material/Add";
+import CancelIcon from "@mui/icons-material/Cancel";
+import APIClient from "../../util/API/APIClient";
+import TextField from "@mui/material/TextField";
+import dayjs from "dayjs";
+import LoadingButton from "@mui/lab/LoadingButton";
+import DateTimePicker from "@mui/lab/DateTimePicker";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+
+interface Props {
+  readonly?: boolean;
+  patientId: string;
+  vaccination?: Vaccination;
+  refreshHandler: () => void;
+  cancelHandler?: () => void;
+}
+
+const VaccinationRow = ({ readonly = false, patientId, vaccination, refreshHandler, cancelHandler }: Props) => {
+  const creating = vaccination === undefined;
+
+  const [editing, setEditing] = useState(creating);
+  const [deleting, setDeleting] = useState(false);
+  const [isPending, setIsPending] = useState(false);
+
+  const [newType, setNewType] = useState(vaccination?.type || "");
+  const [newDate, setNewDate] = useState(vaccination?.dateTime || dayjs());
+
+  const handleEditSave = async () => {
+    setIsPending(true);
+
+    if (vaccination === undefined) throw Error("No existing vaccination");
+    vaccination.type = newType;
+    vaccination.dateTime = newDate;
+    await APIClient.profiles.vaccination.update(vaccination);
+
+    setIsPending(false);
+    setEditing(false);
+    refreshHandler();
+  };
+
+  const handleDelete = async () => {
+    setIsPending(true);
+
+    if (vaccination?.id) await APIClient.profiles.vaccination.delete(vaccination.id);
+
+    setIsPending(false);
+    refreshHandler();
+  };
+
+  const handleCreate = async () => {
+    setIsPending(true);
+
+    await APIClient.profiles.vaccination.add({ patientUserId: patientId, type: newType, dateTime: newDate });
+    setIsPending(false);
+    handleCancel();
+    refreshHandler();
+  };
+
+  const handleCancel = () => {
+    if (creating && cancelHandler) cancelHandler();
+    else setEditing(false);
+  };
+
+  const validate = () => newType.length > 0;
+
+  return (
+    <TableRow hover>
+      {editing ? (
+        <>
+          <TableCell>
+            <TextField
+              id="vaccination-type-input"
+              label="Type"
+              variant="outlined"
+              margin="dense"
+              value={newType}
+              onChange={(e) => setNewType(e.target.value)}
+            />
+          </TableCell>
+          <TableCell>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateTimePicker
+                ampm={false}
+                label="Date"
+                value={newDate}
+                onChange={(date) => setNewDate(date || dayjs())}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </TableCell>
+          <TableCell align="right">
+            <Stack direction="column" justifyContent="center">
+              <Button onClick={handleCancel} startIcon={<CancelIcon />}>
+                Cancel
+              </Button>
+              <LoadingButton
+                disabled={!validate()}
+                loading={isPending}
+                variant="text"
+                loadingPosition="start"
+                startIcon={creating ? <AddIcon /> : <SaveIcon />}
+                onClick={creating ? handleCreate : handleEditSave}
+              >
+                {creating ? "Add" : "Save"}
+              </LoadingButton>
+            </Stack>
+          </TableCell>
+        </>
+      ) : (
+        <>
+          <TableCell>{vaccination?.type}</TableCell>
+          <TableCell>{vaccination?.dateTime?.format("DD/MM/YYYY, HH:mm").toString()}</TableCell>
+          {readonly ? null : (
+            <>
+              <TableCell align="right">
+                <IconButton
+                  onClick={() => {
+                    setEditing(true);
+                  }}
+                >
+                  <EditIcon />
+                </IconButton>
+                <IconButton onClick={() => setDeleting(true)}>
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
+              <Dialog open={deleting} onClose={() => setDeleting(false)}>
+                <DialogTitle>Confirm Deletion</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    Are you sure you want to delete the vaccination <strong>{vaccination?.type}</strong>?
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={() => setDeleting(false)}>No</Button>
+                  <Button onClick={handleDelete} autoFocus>
+                    Yes
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </>
+          )}
+        </>
+      )}
+    </TableRow>
+  );
+};
+
+export default VaccinationRow;
