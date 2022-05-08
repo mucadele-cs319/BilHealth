bfc0f2a67956a8cb38b8d0215a9f8dc3fa26842a
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-05-08 21:13:31 +0300

    Update Model/Dto/ReportDto.cs
    Co-authored-by: Ved <dev@vedat.xyz>

--- Model/Dto/ReportDto.cs
+++ Model/Dto/ReportDto.cs
@@ -5,14 +5,10 @@ namespace BilHealth.Model.Dto
 {
     public class CaseReportDto
     {
-        public Guid Id { get; set; }
         public Instant DateTime { get; set; }
         public string Title { get; set; } = null!;
-        public Guid PatientUserId { get; set; }
-        public Guid? DoctorUserId { get; set; } = null;
         public string? Diagnosis { get; set; } = null!;
         public CaseType Type { get; set; }
-        public CaseState State { get; set; }
         public List<Prescription>? Prescriptions { get; set; } = new();
     }
 }
