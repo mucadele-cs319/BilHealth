8164e7c5bc87bcc29dcbc2d5c43a114af7a28359
Ved <dev@vedat.xyz>
2022-05-10 10:51:09 +0300

    Client: Dayjs Duration plugin

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -7,10 +7,12 @@ import { UserContextProvider } from "./components/UserContext";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import duration from "dayjs/plugin/duration";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault("Europe/Istanbul");
+dayjs.extend(duration)
 
 ReactDOM.render(
   <React.StrictMode>
