0e2bb173d7ed38d437252f7a1c51c6a0527cc08a
Ved <dev@vedat.xyz>
2022-05-12 14:41:47 +0300

    NotificationsListPage: Describe notifications

--- ClientApp/src/components/pages/NotificationsListPage.tsx
+++ ClientApp/src/components/pages/NotificationsListPage.tsx
@@ -31,10 +31,10 @@ const NotificationsListPage = () => {
     <Grid container justifyContent="center">
       <Fade in={true}>
         <Grid item lg={10} xs={11}>
-          <NotificationControlCard />
+          <NotificationControlCard refreshHandler={refreshNotifications} />
           {isLoaded ? (
             notifications.length !== 0 ? (
-              notifications.map((notification, i) => <NotificationItem key={i} data={notification} />)
+              notifications.map((notification, i) => <NotificationItem key={i} notification={notification} refreshHandler={refreshNotifications} />)
             ) : (
               <Stack alignItems="center" className="mt-8">
                 <Typography color="text.secondary">No notifications at this time.</Typography>
